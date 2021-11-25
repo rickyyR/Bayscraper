9537ff7c4fd648cd9e8cca58f1b83b15f853cde0
rickyyR <88052557+rickyyR@users.noreply.github.com>
2021-11-25 12:07:52 +0100

    Added logging

---
+++ src/main/java/org/rickyyr/Bayscraper/scraper/Logger.java
@@ -0,0 +1,107 @@
+package org.rickyyr.Bayscraper.scraper;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class Logger {
+
+  private StringBuilder stringBuilder;
+  private FileWriter fileWriter;
+  private BufferedWriter bufferedWriter;
+  private DateTimeFormatter formatter;
+  private String[] currentDateAndTime;
+
+  public Logger(String fileName) {
+    this.stringBuilder = new StringBuilder();
+    try {
+      this.fileWriter = new FileWriter(fileName, true);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    this.bufferedWriter = new BufferedWriter(this.fileWriter);
+
+    this.formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+      .withLocale(Locale.getDefault())
+      .withZone(ZoneId.systemDefault());
+
+    String dateAndTime = formatter.format(Instant.now());
+    String[] splitted = dateAndTime.split(",");
+    this.currentDateAndTime = new String[]{splitted[0].replaceAll("\\s+", ""),
+      splitted[1].replaceAll("\\s+", "")};
+  }
+
+  public void logSuccessfulScan(String searchword, String numberOfItemsScraped) {
+
+    this.stringBuilder.append("SUCCESS! ")
+      .append(this.currentDateAndTime[0])
+      .append(" - ")
+      .append(this.currentDateAndTime[1])
+      .append(". [")
+      .append(searchword)
+      .append("]. Scraped ")
+      .append(numberOfItemsScraped)
+      .append(" Items.")
+      .append("\n");
+
+    System.out.println(stringBuilder);
+
+    try {
+      this.bufferedWriter.write(this.stringBuilder.toString());
+      this.stringBuilder.setLength(0);
+      this.bufferedWriter.close();
+      this.fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void logScanWithException(String searchword, Exception thrownException) {
+
+    this.stringBuilder.append("FAILED! ")
+      .append(this.currentDateAndTime[0])
+      .append(" - ")
+      .append(this.currentDateAndTime[1])
+      .append(". [")
+      .append(searchword)
+      .append("]. Exception: ")
+      .append(thrownException.toString())
+      .append("\n");
+
+    try {
+      this.bufferedWriter.write(this.stringBuilder.toString());
+      this.stringBuilder.setLength(0);
+      this.bufferedWriter.close();
+      this.fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void logEmptyOrBlockedScan(String searchword) {
+
+    this.stringBuilder.append("FAILED! ")
+      .append(this.currentDateAndTime[0])
+      .append(" - ")
+      .append(this.currentDateAndTime[1])
+      .append(". [")
+      .append(searchword)
+      .append("]. No items found or scan blocked")
+      .append("\n");
+
+    try {
+      this.bufferedWriter.write(this.stringBuilder.toString());
+      this.stringBuilder.setLength(0);
+      this.bufferedWriter.close();
+      this.fileWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
--- src/main/java/org/rickyyr/Bayscraper/spring/ServletRunner.java
+++ src/main/java/org/rickyyr/Bayscraper/spring/ServletRunner.java
@@ -14,17 +14,28 @@ public class ServletRunner {
   @GetMapping(path="/")
   public String scraperService(@RequestParam(value = "scrape", defaultValue = "")String scrape) {
 
+
+
+    Logger logger = new Logger("testLog.txt");
+
+
+
     if(!scrape.equals("")){
 
       ArrayList<ListingItem> items = bayscraper.getItemsForSearchword(browser.getBrowser(), scrape);
 
       if(items.size() > 0) {
 
+        logger.logSuccessfulScan(scrape, String.valueOf(items.size()));
+
         return "<pre>" + Ui.printLogo().replaceAll("\n", "<br>") + "<br>" +
           "Scan for " + "[" + scrape + "]" + " complete!" + "<br>" + "Scraped " + items.size() + " items." + "<br>" +
          "DONE! Please wait 5 minutes before scanning again to avoid getting blocked." + "</pre>";
 
       } else {
+
+        logger.logEmptyOrBlockedScan(scrape);
+
         return "<pre>" + Ui.printLogo().replaceAll("\n", "<br>") + "<br>" +
           "-ERROR! No items found for " + "[" + scrape + "]" + " or scan blocked. Try again in a bit.";
       }
